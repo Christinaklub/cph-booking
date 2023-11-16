05cfff771a85d60c1276406e9750363f140c912f
Hannah West <hannahtan.west@gmail.com>
2023-11-16 09:18:37 +0100

    ye

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
-import Login from "./components/Login";
+
 
 export default function App({ Component, pageProps }) {
   return (
