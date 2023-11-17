9e2874c0d05c3c84d184989a6975c2bbc8417b6e
Jannah <jannahkas@gmail.com>
2023-11-17 16:54:58 +0100

    burde work nu

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,7 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
-import DefaultLayout from ".@/components/layout/DefaultLayout";
-import Login from "../components/Login";
+import DefaultLayout from "../components/layout/DefaultLayout";
 
 export default function App({ Component, pageProps }) {
   return (
