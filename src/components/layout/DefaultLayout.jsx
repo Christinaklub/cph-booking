910466819af9429292acb0da1c825096e1741468
Christina <cph-ck318@cphbusiness.dk>
2023-11-20 12:51:42 +0100

    condendering condistions

--- src/components/layout/DefaultLayout.jsx
+++ src/components/layout/DefaultLayout.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Footer from '../molecules/Footer'
 import Header from '../molecules/Header'
+import { useRouter } from 'next/router'
 
 export default function DefaultLayout({children}) {
+  const router = useRouter()
   return (
+  
     <div>
-        <Header/>
+{/* conditional rendering der kigger på hvor man befinder sig. for er du på andet en /login viser den din header og footer */}
+        {router.pathname !== "/login" && <Header/>}
         {children}
-        <Footer/>
+        {router.pathname !== "/login" && <Footer/> }
     </div>
   )
 }
