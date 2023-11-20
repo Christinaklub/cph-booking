c98e95b77e157dfb6475cdcc63de477b4453ae06
Christina <cph-ck318@cphbusiness.dk>
2023-11-20 11:30:48 +0100

    supabase tables

---
+++ src/components/molecules/SupabaseTables.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import {createClient} from '@supabase/supabase-js';
+
+const addBooking = async (Stilling, Campus, Navn, Email, Lokaletype, Dato, Tidspunkt) => {
+    try {
+      const { data, error } = await supabase
+        .from('bookingTable') // Navnet på vores tabell i databasen
+        .insert([{ Stilling, Campus, Navn, Email, Lokaletype, Dato, Tidspunkt }]);
+      
+      if (error) {
+        // Håndter feil her
+        console.error(error);
+      } else {
+        // Data er lagt til vellykket
+        console.log('Data lagt til:', data);
+      }
+    } catch (error) {
+      console.error('Feil under lagring av data:', error.message);
+    }
+  };
+  
