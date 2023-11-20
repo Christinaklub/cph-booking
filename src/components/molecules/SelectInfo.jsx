85c0c343f15396e8f23d871422e55fe2400285f2
Jannah <jannahkas@gmail.com>
2023-11-20 13:05:30 +0100

    ændringer

--- src/components/molecules/SelectInfo.jsx
+++ src/components/molecules/SelectInfo.jsx
@@ -1,15 +1,60 @@
 import { Carousel } from '@mantine/carousel';
-import classes from './Demo.module.css';
+import classes from './SelectInfo.module.css';
 
 function SelectInfo() {
   return (
-    <Carousel height={200} classNames={classes}>
-      <Carousel.Slide>1</Carousel.Slide>
+    <Carousel height={400} classNames={classes.carousel}>
+      <Carousel.Slide>
+        <div className={classes.slideContent}>
+          <div className={classes.orangeSquare}>
+            <h2 className={classes.title}>Vælg venligst følgende informationer</h2>
+            <div className={classes.dropdownContainer}>
+            <label htmlFor="campus" className={classes.label}>
+            <span className={classes.required}>*</span>Campus:
+            </label>
+              <select id="campus" className={classes.dropdown}>
+                <option value="Lyngby">Lyngby</option>
+                <option value="Søerne">Søerne</option>
+                <option value="City">City</option>
+                <option value="Hillerød">Hillerød</option>
+              </select>
+            </div>
+            <div className={classes.dropdownContainer}>
+              <label htmlFor="stilling" className={classes.label}>
+                <span className={classes.required}> *</span>Stilling:
+              </label>
+              <select id="stilling" className={classes.dropdown}>
+                <option value="studerende">Studerende</option>
+                <option value="undervisere">Undervisere</option>
+              </select>
+            </div>
+            <div className={classes.dropdownContainer}>
+              <label htmlFor="lokale" className={classes.label}>
+                Lokale type:
+              </label>
+              <select id="lokale" className={classes.dropdown}>
+                <option value="" disabled selected hidden>Vælg lokale</option>
+                <option value="mødelokale">Mødelokale</option>
+                <option value="undervisningslokale">Undervisningslokale</option>
+                <option value="zoom-møde">Zoom-møde</option>
+              </select>
+            </div>
+            <div className={classes.dropdownContainer}>
+              <label htmlFor="tidspunkt" className={classes.label}>
+                Tidsinterval:
+              </label>
+              <select id="tidspunkt" className={classes.dropdown}>
+                <option value="" disabled selected hidden>Vælg tidspunkt</option>
+                {/* husk at tilføje valg af tidspunkt */}
+              </select>
+            </div>
+            <button className={classes.button}>Næste</button>
+          </div>
+        </div>
+      </Carousel.Slide>
       <Carousel.Slide>2</Carousel.Slide>
-      <Carousel.Slide>3</Carousel.Slide>
-      {/* ...other slides */}
     </Carousel>
   );
 }
 
-export default SelectInfo
+export default SelectInfo;
--- src/pages/booking/index.js
+++ src/pages/booking/index.js
@@ -1,7 +1,9 @@
+import SelectInfo from '@/components/molecules/SelectInfo'
 import React from 'react'
 
 export default function Booking() {
   return (
-    <div>Booking</div>
+    <SelectInfo></SelectInfo>
+    
   )
 }
