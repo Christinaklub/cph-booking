ea9c146d1b46f4fc64fc05ea78e4ff54543e4602
Hannah West <hannahtan.west@gmail.com>
2023-11-22 10:56:05 +0100

    booking

---
+++ src/components/molecules/SelectDate.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './SelectInfo.module.css'
+
+export default function SelectDate() {
+  return (
+    <div className={classes.slideContent}>
+        <div className={classes.orangeSquare}>
+        </div>
+    </div>
+  )
+}
--- src/pages/booking/index.js
+++ src/pages/booking/index.js
@@ -1,9 +1,21 @@
 import SelectInfo from '@/components/molecules/SelectInfo'
+import SelectDate from '@/components/molecules/SelectDate'
 import React from 'react'
+import { Carousel } from '@mantine/carousel';
+import '@mantine/carousel/styles.css';
+import styles from './/index.module.css'
+
+
 
 export default function Booking() {
   return (
-    <SelectInfo></SelectInfo>
-    
-  )
+    <div className={styles.container}>
+    <Carousel slideSize="100%" height={700} draggable={false}>
+      <Carousel.Slide><SelectInfo></SelectInfo></Carousel.Slide>
+      <Carousel.Slide><SelectDate></SelectDate></Carousel.Slide>
+      <Carousel.Slide></Carousel.Slide>
+    </Carousel>
+    </div>
+  );
 }
+
