30c733616d21736f47fae6fc929774c1a24c73ff
Christina <cph-ck318@cphbusiness.dk>
2023-11-22 14:32:58 +0100

    hei

--- src/components/molecules/InfoAvailable.jsx
+++ src/components/molecules/InfoAvailable.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { supabase } from './supabase'; 
+import {createClient} from '@supabase/supabase-js';
+
 
 
 //komponent om tilgjenglihet til tider og lokaler - vet ikke hva vi skal gjøre mer jeg er lost
