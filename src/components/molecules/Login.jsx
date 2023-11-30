c6825959d3d3cf84c45904eccd6a32f6e050941f
Christina <cph-ck318@cphbusiness.dk>
2023-11-30 12:32:10 +0100

    login ferdig

--- src/components/molecules/Login.jsx
+++ src/components/molecules/Login.jsx
@@ -11,8 +11,33 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isLoading, setIsLoading] = useState(false);
+    const [ loginError, setError] = useState (false);
 
     const supabase = createClient('https://txxxtrswrqluxdohetsm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4eHh0cnN3cnFsdXhkb2hldHNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAwNTE4MjIsImV4cCI6MjAxNTYyNzgyMn0.c5FcVULcw1G-IULur56wxb3wnZZVXz6nkfAmOm1Sipc');
+
+    const handleLogin = async () => {
+            let authorized = false;
+            setError(false);
+
+                const { data, error } = await supabase.auth.signInWithPassword({
+                    email: email,
+                    password: password,
+                });
+                console.log (error, data);
+                setIsLoading(false);
+    
+                if (data && data.user) {
+                    // Gemme brukerinfo i kontekst eller gjøre noe annet
+                    console.log(data.user.email);
+                    window.location.href = "/booking";
+                } else {
+                    setError(true);
+                }
+                console.log("data", data);
+                console.log("Error", error);
+    }
+
+
 
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
@@ -25,90 +50,26 @@ const Login = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         setIsLoading(true);
-        // Here you can call your signup API or method
         console.log('Logger ind med', email, password);
-        loginNewUser();
+        handleLogin ();
+        // loginNewUser();
         // redirecter dig til booking side når dit login er valideret 
-        window.location.href = "/booking";
     }
 
-    async function loginNewUser() {
-        const { data, error } = await supabase.auth.signInWithPassword({
-          email: email,
-          password: password,
-        //   options: {
-        //     redirectTo: 'https//example.com/welcome'
-        //   }
-        })
-        setIsLoading(false);
+    // async function loginNewUser() {
+    //     const { data, error } = await supabase.auth.signInWithPassword({
+    //       email: email,
+    //       password: password,
+    //     })
+    //     setIsLoading(false);
 
-        if (data && data.user) {
-            // Gemme user info i context.
-            console.log(data.user.email);
-        }
-        console.log("data", data);
-        console.log("Error", error);
-
-
-// Denne funksjonen virker ikke fordi den prøver å connecte supabase manuelt som ikke gir noen mening 
-            const handleLogin = async () => {
-                try {
-                
-                if (
-                    (email === 'cph-nj247@cphbusiness.dk' && password === 'Cph18868') ||
-                    (email === 'cph-st255@cphbusiness.dk' && password === 'cph51943')
-                ) {
-                    // Autentisering vellykket
-                    console.log('Innlogging vellykket for autorisert bruker:', email);
-                } else {
-                    // Feil e-postadresse eller passord
-                    console.error('Feil e-postadresse eller passord.');
-                }
-                } catch (error) {
-                console.error('Feil ved indlogging:', error.message);
-                }
-            };
-
-            // Dette er n funksjonn christina skal fikse men hun må finne ud av ting 
-            // const handleLogin = async () => {
-            //     try {
-            //         let authorized = false;
-            
-            //         // Sjekk om den innloggede brukeren er en av de autoriserte brukerne
-            //         if (
-            //             (email === 'cph-nj247@cphbusiness.dk' && password === 'Cph18868') ||
-            //             (email === 'cph-st255@cphbusiness.dk' && password === 'cph51943')
-            //         ) {
-            //             // Setter variabelen til sann hvis innloggingen er fra en autorisert bruker
-            //             authorized = true;
-            //         }
-            
-            //         // Hvis innloggingen kommer fra en autorisert bruker, logg inn med Supabase
-            //         if (authorized) {
-            //             const { data, error } = await supabase.auth.signInWithPassword({
-            //                 email: email,
-            //                 password: password,
-            //             });
-            
-            //             setIsLoading(false);
-            
-            //             if (data && data.user) {
-            //                 // Gemme brukerinfo i kontekst eller gjøre noe annet
-            //                 console.log(data.user.email);
-            //             }
-            //             console.log("data", data);
-            //             console.log("Error", error);
-            //         } else {
-            //             // Hvis ikke en autorisert bruker, gi en feilmelding eller utfør en passende handling
-            //             console.error('Feil e-postadresse eller passord.');
-            //         }
-            //     } catch (error) {
-            //         console.error('Feil ved innlogging:', error.message);
-            //     }
-            // };
-            
-
-      }
+    //     if (data && data.user) {
+    //         // Gemme user info i context.
+    //         console.log(data.user.email);
+    //     }
+    //     console.log("Data", data);
+    //     console.log("Error", error);
+    //   }
 
     return (
         <div className={styles.container} >
@@ -122,16 +83,13 @@ const Login = () => {
                 <input className={styles.margin} type="email" value={email} onChange={handleEmailChange} required placeholder='Email' />
             </label>
             <br />
-            <label>
+            <label> 
                 
-                <input className={styles.margin} type="password" value={password} onChang={handlePasswordChange}
-
-                    // onChange={(event) =>{
-                    //     handlePasswordChange(event);
-                    //     handleLogin(); 
-                    //     }}
-                     required placeholder='Adgangskode'/>
+                <input className={styles.margin} type="password" value={password} 
+                    onChange={handlePasswordChange} required placeholder='Adgangskode'/>
             </label>
+            {/* condistinal rendering på p tag, dette blir vist når login error er true ellers er den bare satt til false*/}
+            {loginError && <p className={styles.loginError}>Forkert indlogging</p>}
             <br />
 
             <Checkbox className={styles.logOff}
