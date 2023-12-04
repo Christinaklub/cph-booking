2bf53fa3de00109bdb49943b7cf0106d3a3d5ecc
Hannah West <hannahtan.west@gmail.com>
2023-12-04 12:57:22 +0100

    rettelser

--- src/components/molecules/Header.jsx
+++ src/components/molecules/Header.jsx
@@ -10,25 +10,25 @@ export default function Header() {
         <Image src={logo} alt="/" width={300}/>
 
         <div className={styles.rightSide}>
-            <Link href="/" className={styles.link}>
+            <Link href="" className={styles.link}>
                 Links
             </Link>
-            <Link href="/" className={styles.link}>
+            <Link href="" className={styles.link}>
                 Mine flows
             </Link>
-            <Link href="/" className={styles.link}>
+            <Link href="" className={styles.link}>
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="26" height="26" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" /><path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" /></svg>
             </Link>
-            <Link href="/" className={styles.link}>
+            <Link href="" className={styles.link}>
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-message-circle-2" width="26" height="26" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 20l1.3 -3.9a9 8 0 1 1 3.4 2.9l-4.7 1" /></svg>
             </Link>
-            <Link href="/" className={styles.link}>
+            <Link href="" className={styles.link}>
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-bell" width="26" height="26" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 5a2 2 0 1 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" /><path d="M9 17v1a3 3 0 0 0 6 0v-1" /></svg>
             </Link>
-            <Link href="/" className={styles.link}>
+            <Link href="" className={styles.link}>
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="26" height="26" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
             </Link>
-            <Link href="/" className={styles.link}>
+            <Link href="" className={styles.link}>
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-settings" width="26" height="26" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" /><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /></svg>
             </Link>
             
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,17 +5,20 @@ import { MantineProvider } from "@mantine/core";
 import DefaultLayout from "../components/layout/DefaultLayout";
 import { MyContext } from "@/context/my-context";
 import { DateProvider } from "@/context/date-context";
-import SelectDate from '../components/molecules/SelectDate';
+
 
 // import { SupabaseAuthClient } from "@supabase/supabase-js/dist/module/lib/SupabaseAuthClient";
 
   export default function App({ Component, pageProps }) {
+
+    // Da vi har data der er dynamisk opretter vi useStates til de værdier der skal kunne ændre sig
     const [campus, setCampus] = useState("");
     const [stilling, setStilling] = useState("");
     const [lokale, setLokale] = useState("");
     const [tidspunkt, setTidspunkt] = useState("");
     const [selectedDate, setSelectedDate] = useState("");
 
+    // Vi smider vores useStates ind i et object vi kalder for contextValue så vi kan hente dataen ind i andre components
   const contextValue = {
     campus,
     setCampus,
